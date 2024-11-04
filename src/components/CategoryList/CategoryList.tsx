9ebcd4588d2c39945d4a9bcd950b9d96e8bf6351
Taras Noga <taraskin2012@gmail.com>
2024-11-04 20:33:15 +0200

    add delete category

--- src/components/CategoryList/CategoryList.tsx
+++ src/components/CategoryList/CategoryList.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import DataTable from 'react-data-table-component';
-import { fetchCategories } from '../../store/categorySlice';
+import { fetchCategories, deleteCategory } from '../../store/categorySlice';
 import styles from './categoryList.module.scss';
 
 const columns = [
@@ -21,20 +21,41 @@ const CategoryList: React.FC = (): JSX.Element => {
   const dispatch = useAppDispatch();
   const categories = useAppSelector((state) => state.category.items);
 
+  const [selectedCategoryId, setSelectedCategoryId] = useState<number | null>(
+    null
+  );
+
   useEffect(() => {
     dispatch(fetchCategories({ page: 0, size: categories.length || 1000 }));
   }, [dispatch, categories.length]);
 
+  const handleRowSelected = (state: any): void => {
+    const selectedId = state.selectedRows[0]?.id as number | undefined;
+    setSelectedCategoryId(selectedId || null);
+  };
+
+  const handleDelete = () => {
+    if (selectedCategoryId !== null) {
+      dispatch(deleteCategory(selectedCategoryId)).then(() => {
+        dispatch(fetchCategories({ page: 0, size: categories.length || 1000 }));
+        setSelectedCategoryId(null);
+      });
+    }
+  };
+
   return (
     <>
       <h3 className={styles.title}>Category List</h3>
-
+      <button onClick={handleDelete} disabled={!selectedCategoryId}>
+        Delete Selected Category
+      </button>
       <DataTable
         columns={columns}
         data={categories}
         selectableRows
         selectableRowsHighlight
         selectableRowsSingle
+        onSelectedRowsChange={handleRowSelected}
         pagination
         fixedHeader={true}
         pointerOnHover
