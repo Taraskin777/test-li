0d444d09cf12252fa77beae5c6e336be8037c53a
Taras Noga <taraskin2012@gmail.com>
2024-11-04 15:30:33 +0200

    add fetch categories

--- src/store/categorySlice.ts
+++ src/store/categorySlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { ICategory } from '../types/category.types';
 
@@ -8,8 +8,46 @@ const initialState: ICategory = {
   totalPages: 0,
 };
 
+export const fetchCategories = createAsyncThunk(
+  'category/fetchCategories',
+  async (
+    { page, size }: { page: number; size: number },
+    { rejectWithValue }
+  ) => {
+    try {
+      const response = await axios.get(
+        `PUBLIC_API_URL/admin/categories?page=${page}&size=${size}`
+      );
+      return response.data;
+    } catch (error: any) {
+      return rejectWithValue(
+        error.response?.data || 'Error fetching categories'
+      );
+    }
+  }
+);
+
 const slice = createSlice({
   name: 'category',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCategories.pending, (state) => {})
+      .addCase(
+        fetchCategories.fulfilled,
+        (state, action: PayloadAction<ICategory>) => {
+          state.items = action.payload.items;
+          state.hasNext = action.payload.hasNext;
+          state.totalPages = action.payload.totalPages;
+        }
+      )
+      .addCase(fetchCategories.rejected, (state, action) => {
+        console.error(action.payload);
+      });
+  },
 });
+
+
+
+export default slice.reducer;
